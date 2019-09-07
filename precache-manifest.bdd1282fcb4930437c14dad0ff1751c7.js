42a5f6c37242047149db2c8d4249ce4b518bd352
hehe100596 <hehe100596@yahoo.com>
2019-09-07 15:56:48 +0200

    Updates

---
+++ precache-manifest.bdd1282fcb4930437c14dad0ff1751c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b167aef0c5d6b6de53ac2de06820eac",
+    "url": "/dp_app/index.html"
+  },
+  {
+    "revision": "994dd65b2cc618c43034",
+    "url": "/dp_app/static/css/main.868a356b.chunk.css"
+  },
+  {
+    "revision": "e02a5abee2ac352f0732",
+    "url": "/dp_app/static/js/2.6a90eaad.chunk.js"
+  },
+  {
+    "revision": "994dd65b2cc618c43034",
+    "url": "/dp_app/static/js/main.0681b5cc.chunk.js"
+  },
+  {
+    "revision": "76178287b4f630823ce7",
+    "url": "/dp_app/static/js/runtime~main.fbec3eb0.js"
+  },
+  {
+    "revision": "dde057f127fc4765ff049c8b3ec99842",
+    "url": "/dp_app/static/media/dice.dde057f1.png"
+  }
+]);
