45f05c38bb94cf37871a798c86c8bba6a329f094
Juraj Bacovcin <hehe100596@yahoo.com>
2020-02-24 16:19:51 +0100

    Deleting previous version.

--- src/js/api.js
+++
@@ -1,9 +0,0 @@
-import openSocket from 'socket.io-client';
-const socket = openSocket('http://localhost:8000');
-
-function subscribeToTimer(offset, cb) {
-	socket.on('timer', timestamp => cb(null, timestamp));
-	socket.emit('subscribeToTimer', { offset: offset, interval: 1000 });
-}
-
-export { subscribeToTimer }
