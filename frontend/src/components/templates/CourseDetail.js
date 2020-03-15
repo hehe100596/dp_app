4c318150dc239131303f17b33daac8daf500e93c
Juraj Bacovcin <hehe100596@yahoo.com>
2020-03-15 19:08:51 +0100

    Adding TODO comment for future work.

--- frontend/src/components/templates/CourseDetail.js
+++ frontend/src/components/templates/CourseDetail.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 export function CourseDetail({ courseId }) {
+  // TODO: Confirm user really has access to this course!
+
   return (
     <div align="center">
       ---
--- frontend/src/components/templates/CourseModification.js
+++ frontend/src/components/templates/CourseModification.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 export function CourseModification({ courseId }) {
+  // TODO: Confirm user really has access to this course!
+
   return (
     <div align="center">
       ---
